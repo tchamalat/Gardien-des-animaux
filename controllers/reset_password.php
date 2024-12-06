bcca26769ef4d075ec3f9ecbcfe2b275760ec619
Dan080404 <dan.bensimon44@gmail.com>
2024-12-06 11:03:09 +0100

    Update reset_password.php

--- controllers/reset_password.php
+++ controllers/reset_password.php
@@ -51,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <img src="images/logo.png" alt="Logo Gardien des Animaux">
             <h1 class="header-slogan">Un foyer chaleureux même en votre absence</h1>
             <div class="auth-buttons">
-                <button class="btn" onclick="window.location.href='index.php'">Accueil</button>
+                <button class="btn" onclick="window.location.href='/index.php'">Accueil</button>
             </div>
         </div>
     </header>
@@ -64,7 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         <?php if (isset($token)): ?>
             <p>Veuillez saisir un nouveau mot de passe pour votre compte.</p>
-            <form action="reset_password.php" method="POST">
+            <form action="/controllers/reset_password.php" method="POST">
                 <input type="hidden" name="token" value="<?php echo htmlspecialchars($token); ?>">
                 <div class="form-group">
                     <label for="new_password">Nouveau mot de passe :</label>
@@ -80,21 +80,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <div>
             <h4>En savoir plus :</h4>
             <ul>
-                <li><a href="securite.php">Sécurité</a></li>
-                <li><a href="aide.php">Centre d'aide</a></li>
+                <li><a href="/views/securite.php">Sécurité</a></li>
+                <li><a href="/views/aide.php">Centre d'aide</a></li>
             </ul>
         </div>
         <div>
             <h4>A propos de nous :</h4>
             <ul>
-                <li><a href="confidentialite.php">Politique de confidentialité</a></li>
-                <li><a href="contact.php">Nous contacter</a></li>
+                <li><a href="/views/confidentialite.php">Politique de confidentialité</a></li>
+                <li><a href="/views/contact.php">Nous contacter</a></li>
             </ul>
         </div>
         <div>
             <h4>Conditions Générales :</h4>
             <ul>
-                <li><a href="conditions.php">Conditions d'utilisateur et de Service</a></li>
+                <li><a href="/views/conditions.php">Conditions d'utilisateur et de Service</a></li>
             </ul>
         </div>
     </div>
