d3fb7b257d71c673017ba58fbcb4733f7e33ed2b
Dan080404 <dan.bensimon44@gmail.com>
2024-12-06 10:37:09 +0100

    Update confirmation.php

--- views/confirmation.php
+++ views/confirmation.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confirmation d'inscription</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="/CSS/styles.css">
 </head>
 <body>
     <header>
@@ -17,7 +17,7 @@
     <div class="form-container">
         <h2>Merci pour votre inscription !</h2>
         <p>Votre compte a été créé avec succès. Vous pouvez maintenant vous connecter.</p>
-        <button class="btn" onclick="window.location.href='login.html'">Se connecter</button>
+        <button class="btn" onclick="window.location.href='/views/login.html'">Se connecter</button>
     </div>
 
     <footer>
@@ -25,21 +25,21 @@
         <div>
             <h4>En savoir plus :</h4>
             <ul>
-                <li><a href="securite.php">Sécurité</a></li>
-                <li><a href="aide.php">Centre d'aide</a></li>
+                <li><a href="/views/securite.php">Sécurité</a></li>
+                <li><a href="/views/aide.php">Centre d'aide</a></li>
             </ul>
         </div>
         <div>
             <h4>A propos de nous :</h4>
             <ul>
-                <li><a href="confidentialite.php">Politique de confidentialité</a></li>
-                <li><a href="contact.php">Nous contacter</a></li>
+                <li><a href="/views/confidentialite.php">Politique de confidentialité</a></li>
+                <li><a href="/views/contact.php">Nous contacter</a></li>
             </ul>
         </div>
         <div>
             <h4>Conditions Générales :</h4>
             <ul>
-                <li><a href="conditions.php">Conditions d'utilisateur et de Service</a></li>
+                <li><a href="/views/conditions.php">Conditions d'utilisateur et de Service</a></li>
             </ul>
         </div>
     </div>
