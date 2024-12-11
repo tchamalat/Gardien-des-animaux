bcb8f4bd7dc5cffddd5679a06743e0474e3241a5
Dan080404 <dan.bensimon44@gmail.com>
2024-12-11 11:49:40 +0100

    Update index_connect_gardien.php

--- index_connect_gardien.php
+++ index_connect_gardien.php
@@ -128,7 +128,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 }
                 ?>
                 <button class="btn" onclick="window.location.href='mes_reservations.php'">Mes réservations</button>
-                <button class="btn" onclick="window.location.href='discussion.php'">Discussion</button>
+                <button class="btn" onclick="window.location.href='discussion_gardien.php'">Discussion</button>
             </div>
         </div>
     </header>
