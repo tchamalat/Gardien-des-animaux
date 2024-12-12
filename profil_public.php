ff13064dde7e7c7f55666386a0c223c458d40122
Dan080404 <dan.bensimon44@gmail.com>
2024-12-12 18:23:05 +0100

    Update profil_public.php

--- profil_public.php
+++ profil_public.php
@@ -16,7 +16,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Gérer l'ajout des animaux
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nom_animal'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ajouter_animaux'])) {
     $noms_animaux = is_array($_POST['nom_animal']) ? $_POST['nom_animal'] : [$_POST['nom_animal']];
     $photos_animaux = $_FILES['photo_animal'];
 
@@ -56,6 +56,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nom_animal'])) {
             die("Erreur de téléchargement de la photo : " . $photos_animaux['error'][$i]);
         }
     }
+
+    $message = "Animaux ajoutés avec succès !";
+}
 
     $message = "Animaux ajoutés avec succès !";
 }
