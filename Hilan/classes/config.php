3fe6c346dea62bfd6e533de1d780d17ac1e6a4db
ATSASSE <hatsasse@gmail.com>
2024-12-15 22:35:56 +0100

    Delete Hilan directory

--- Hilan/classes/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('DBUSER', "root");
-define('DBPASS', "root");
-define('DBNAME', "mychat_db");
-define('DBHOST', "Localhost");
--- Hilan/includes/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if( isset($_SESSION['userid']))
-{
-    unset($SESSION['userid']);
-}
-
-$info->logged_in = false;
-echo json_encode($info);
