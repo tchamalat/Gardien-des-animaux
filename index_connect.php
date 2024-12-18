2cf388cb6e289ce48b5172901336a87e0ecb02f3
Dan080404 <dan.bensimon44@gmail.com>
2024-12-18 10:13:53 +0100

    Update index_connect.php

--- index_connect.php
+++ index_connect.php
@@ -69,11 +69,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <!-- Hero Section -->
     <section class="hero">
         <img src="images/premierplan.png" alt="Un foyer chaleureux">
-        <div class="hero-text">
-            <div class="hero-buttons">
-                <button class="btn btn-hero" onclick="window.location.href='search_page.php'">Trouver un gardien</button>
-                <button class="btn btn-hero" onclick="window.location.href='discussion.php'">Discussion</button>
-            </div>
+        <div class="hero-buttons">
+            <button class="btn btn-hero" onclick="window.location.href='search_page.php'">Trouver un gardien</button>
+            <button class="btn btn-hero" onclick="window.location.href='discussion.php'">Discussion</button>
         </div>
     </section>
 
