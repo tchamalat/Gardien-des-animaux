a95092d41d46bf9acac2f275faf2cf77b9a0375e
Dan080404 <dan.bensimon44@gmail.com>
2024-12-21 15:24:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ include 'config.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gardien des Animaux</title>
-    <link rel="stylesheet" href="styles.css">
     <style>
         * {
             margin: 0;
