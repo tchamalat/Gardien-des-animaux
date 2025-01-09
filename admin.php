e057027ca6e7d57c882b2d63562fbb0d2890a104
Dan080404 <dan.bensimon44@gmail.com>
2025-01-09 18:11:10 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -81,14 +81,12 @@ try {
         }
 
         .dashboard-container {
-            max-width: 1200px;
-            margin: 120px auto;
-            background: rgba(255, 255, 255, 0.95);
-            border-radius: 15px;
-            padding: 30px;
-            box-shadow: 0 8px 15px rgba(0, 0, 0, 0.2);
+            flex: 1; /* Pousse le footer vers le bas */
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
         }
-
         .dashboard-header {
             text-align: center;
             margin-bottom: 30px;
