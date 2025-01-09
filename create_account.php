cf5ea7b932a9e0d4f8212f4623a7ff6a0a392c70
Dan080404 <dan.bensimon44@gmail.com>
2025-01-09 16:56:09 +0100

    Update create_account.php

--- create_account.php
+++ create_account.php
@@ -427,7 +427,7 @@
     <script>
         // Fonction de validation pour un mot de passe sécurisé
         function validatePassword(password) {
-            const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+            const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,}$/;
             return passwordRegex.test(password);
         }
 
