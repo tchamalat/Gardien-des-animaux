4350fef7743d2c3be05dddf2202dc7c94aa8954d
Dan080404 <dan.bensimon44@gmail.com>
2025-01-14 14:55:38 +0100

    Update subscriptions.php

--- subscriptions.php
+++ subscriptions.php
@@ -247,8 +247,30 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] != 1) {
     </main>
 
     <footer>
-        <p>&copy; 2025 Gardien des Animaux. Tous droits réservés.</p>
+        <div class="footer-links">
+            <div>
+                <h4>En savoir plus :</h4>
+                <ul>
+                    <li><a href="securite_connect.php">Sécurité</a></li>
+                    <li><a href="aide_connect.php">Centre d'aide</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>A propos de nous :</h4>
+                <ul>
+                    <li><a href="confidentialite_connect.php">Politique de confidentialité</a></li>
+                    <li><a href="contact_connect.php">Nous contacter</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>Conditions Générales :</h4>
+                <ul>
+                    <li><a href="conditions_connect.php">Conditions d'utilisateur et de Service</a></li>
+                </ul>
+            </div>
+        </div>
     </footer>
+
 
     <script>
         function subscribe(type) {
