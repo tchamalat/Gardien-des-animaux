7bfb10e46b59ab7bb2df26ef12456f829ba119fb
Dan080404 <dan.bensimon44@gmail.com>
2025-01-16 22:17:38 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -274,6 +274,16 @@ $abonnementChartData = transformDataForChart($abonnementEvolution);
             color: orange;
         }
     </style>
+    <script>
+        window.addEventListener('scroll', () => {
+            const header = document.querySelector('header');
+            if (window.scrollY > 50) {
+                header.classList.add('scrolled');
+            } else {
+                header.classList.remove('scrolled');
+            }
+        });
+    </script>
 </head>
 <body>
 
