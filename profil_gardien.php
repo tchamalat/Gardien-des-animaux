48f2b9bb77f43268e4794cc1b6acf151f6acbc60
Dan080404 <dan.bensimon44@gmail.com>
2025-01-16 17:04:22 +0100

    Update profil_gardien.php

--- profil_gardien.php
+++ profil_gardien.php
@@ -362,6 +362,16 @@ $conn->close();
             border-radius: 5px;
         }
     </style>
+    <script>
+        window.addEventListener('scroll', () => {
+            const header = document.querySelector('header');
+            if (window.scrollY > 50) {
+                header.classList.add('scrolled');
+            } else {
+                header.classList.remove('scrolled');
+            }
+        });
+    </script>
 </head>
 <body>
 
