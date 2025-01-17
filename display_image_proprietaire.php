d1bab20af17fa39c605f4bb092eca789a8783d3b
Dan080404 <dan.bensimon44@gmail.com>
2025-01-17 14:32:34 +0100

    Update display_image_proprietaire.php

--- display_image_proprietaire.php
+++ display_image_proprietaire.php
@@ -1,15 +1,12 @@
 <?php
 include 'config.php';
 
-// Vérifiez si un ID utilisateur est passé dans l'URL
 if (!isset($_GET['id'])) {
     header("HTTP/1.1 400 Bad Request");
     exit("ID de propriétaire manquant.");
 }
 
 $proprietaire_id = intval($_GET['id']);
-
-// Récupérer les données de l'image depuis la base de données pour les propriétaires (role = 1)
 $sql = "SELECT profile_picture FROM creation_compte WHERE id = ? AND role = 1";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $proprietaire_id);
@@ -18,14 +15,12 @@ $stmt->bind_result($profile_picture);
 $stmt->fetch();
 $stmt->close();
 
-// Vérifier si une image existe
 if ($profile_picture) {
-    header("Content-Type: image/jpeg"); // Adapter le type MIME si nécessaire
+    header("Content-Type: image/jpeg");
     echo $profile_picture;
 } else {
-    // Si aucune image n'est définie, afficher une image par défaut
     header("Content-Type: image/png");
-    readfile("images/default_owner.png"); // Remplacez par l'image par défaut pour les propriétaires
+    readfile("images/default_owner.png"); 
 }
 
 $conn->close();
