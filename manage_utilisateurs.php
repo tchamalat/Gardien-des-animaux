381ffcae310dcb8a54e9bf972940a272b8d04d5f
Dan080404 <dan.bensimon44@gmail.com>
2025-01-17 21:41:49 +0100

    Update manage_utilisateurs.php

--- manage_utilisateurs.php
+++ manage_utilisateurs.php
@@ -187,7 +187,6 @@ $result = $conn->query("SELECT * FROM creation_compte");
     <h2>Menu Admin</h2>
     <ul>
         <li><a href="dashboard.php">Tableau de Bord</a></li>
-        <li><a href="manage_abonnements.php">Gérer les Abonnements</a></li>
         <li><a href="manage_utilisateurs.php" class="active">Gérer les Utilisateurs</a></li>
         <li><a href="manage_reservations.php">Gérer les Réservations</a></li>
         <li><a href="manage_avis.php">Gérer les Avis</a></li>
