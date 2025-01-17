b73755f0864f7f79143dbbc53b00e42a9d13a1dc
ATSASSE <hatsasse@gmail.com>
2025-01-17 13:57:52 +0100

    Add files via upload

---
+++ messages/classes/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('DBUSER', "gardien");
+define('DBPASS', "G@rdien-des-chiens");
+define('DBNAME', "gardiendb");
+define('DBHOST', "82.66.52.68");
---
+++ messages/classes/database.php
@@ -0,0 +1,162 @@
+<?php
+
+
+class Database
+{
+    private $con;
+
+    function __construct()
+    {
+
+        $this->con = $this->connect();
+    }
+
+    private function connect()
+        {
+
+            $string = "mysql:host=82.66.52.68;dbname=gardiendb";            try{
+
+                $connection = new PDO($string, DBUSER, DBPASS);
+                return $connection;
+
+            }catch(PDOException $e)
+            {
+
+                echo $e->getMessage();
+                die;
+            }
+
+            return false;
+            
+
+        }
+// write to database
+        public function write ($query,$data_array = [])
+        {
+
+            try
+            {
+                $con = $this->connect();
+                $statement = $con->prepare($query);
+
+                foreach ($data_array as $key => $value){
+
+                    $statement->bindValue(':' .$key, $value);
+
+
+            }
+            
+
+           $check = $statement->execute();
+        }catch(PDOException $e)
+        {
+
+            echo $e->getMessage();
+            
+        }
+           
+           if($check)
+           {
+            return true;
+           }
+           return false;
+        }
+
+        // read From database
+        public function read($query,$data_array = [])
+        {
+
+            try
+            {
+                $con = $this->connect();
+                $statement = $con->prepare($query);
+
+                foreach ($data_array as $key => $value){
+
+                    $statement->bindValue(':' .$key, $value);
+
+
+            }
+            
+
+           $check = $statement->execute();
+        }catch(PDOException $e)
+        {
+
+            echo $e->getMessage();
+            
+        }
+           
+           if($check)
+           {
+            $result = $statement->fetchAll(PDO::FETCH_OBJ);
+            if (is_array($result) && count($result) > 0)
+            {
+                return $result;
+            }
+            return false;
+           }
+           return false;
+        }
+
+        public function get_user($userid)
+        {
+
+            try
+            {
+                $con = $this->connect();
+                $arr['userid'] = $userid;
+                $query = "select * from users where userid = :userid limit 1";
+                $statement = $con->prepare($query);
+
+                foreach ($userid as $key => $value){
+
+                    $statement->bindValue(':' .$key, $value);
+
+
+            }
+            
+
+           $check = $statement->execute($arr);
+        }catch(PDOException $e)
+        {
+
+            echo $e->getMessage();
+            
+        }
+           
+           if($check)
+           {
+            $result = $statement->fetchAll(PDO::FETCH_OBJ);
+            if (is_array($result) && count($result) > 0)
+            {
+                return $result[0];
+            }
+            return false;
+           }
+           return false;
+        }
+
+
+       
+
+       
+
+        //read from database
+        
+        public function generate_id($max)
+        {
+            $rand = "";
+            $rand_count = rand (4,$max);
+            for ($i=0; $i < $rand_count ; $i++) { 
+
+                # code...
+                $r = rand(0,9);
+                $rand .= $r;
+            }
+
+            return $rand;
+        }
+    
+}
+
