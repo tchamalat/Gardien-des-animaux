15131dd2d9d3d3c210767f04c040acf7a728128c
Roman <59027103+tchamalat@users.noreply.github.com>
2025-01-17 15:26:33 +0100

    Update profil_public.php

--- profil_public.php
+++ profil_public.php
@@ -233,7 +233,6 @@ $stmt_animaux->close();
 
         .animal-card img {
             width: 100%;
-            height: 150px;
             object-fit: cover;
             border-radius: 10px;
             margin-bottom: 15px;
