0a63eb650d25bf941275256372de44222430a969
Roman <59027103+tchamalat@users.noreply.github.com>
2025-01-17 14:44:03 +0100

    Update create_account.php

--- create_account.php
+++ create_account.php
@@ -90,7 +90,6 @@
             border-radius: 10px;
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
             color: #333;
-            margin-top: 50px;
         }
 
         .form-container h2 {
