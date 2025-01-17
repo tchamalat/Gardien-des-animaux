dfbee95b6b49a34dfd3d9a4989cd3e0663f4b3e1
Dan080404 <dan.bensimon44@gmail.com>
2025-01-17 21:41:19 +0100

    Update manage_faq.php

--- manage_faq.php
+++ manage_faq.php
@@ -186,7 +186,6 @@ $result = $conn->query("SELECT FAQ.id_faq, FAQ.question, FAQ.reponse, Administra
     <h2>Menu Admin</h2>
     <ul>
         <li><a href="admin.php">Tableau de Bord</a></li>
-        <li><a href="manage_abonnements.php">Gérer les Abonnements</a></li>
         <li><a href="manage_utilisateurs.php">Gérer les Utilisateurs</a></li>
         <li><a href="manage_reservations.php">Gérer les Réservations</a></li>
         <li><a href="manage_avis.php">Gérer les Avis</a></li>
