a3ccb6476e6813cddebba14be7fb7f63add3c22b
Dan080404 <dan.bensimon44@gmail.com>
2025-01-17 21:41:01 +0100

    Update manage_avis.php

--- manage_avis.php
+++ manage_avis.php
@@ -188,7 +188,6 @@ $result = $conn->query("SELECT * FROM avis");
     <h2>Menu Admin</h2>
     <ul>
         <li><a href="admin.php">Tableau de Bord</a></li>
-        <li><a href="manage_abonnements.php">Gérer les Abonnements</a></li>
         <li><a href="manage_utilisateurs.php">Gérer les Utilisateurs</a></li>
         <li><a href="manage_reservations.php">Gérer les Réservations</a></li>
         <li><a href="manage_avis.php" class="active">Gérer les Avis</a></li>
