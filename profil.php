e7c1e756a99cd87eb29a1c64f484e2e54b086445
Dan080404 <dan.bensimon44@gmail.com>
2025-01-17 14:40:58 +0100

    Update profil.php

--- profil.php
+++ profil.php
@@ -30,7 +30,6 @@ unset($_SESSION['message']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Profil - Gardien des Animaux</title>
     <style>
-        /* Styles globaux */
         * {
             margin: 0;
             padding: 0;
