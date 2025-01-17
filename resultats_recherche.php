1101ab2ec6fb6a8895dd50d3ce120db63cded983
Dan080404 <dan.bensimon44@gmail.com>
2025-01-17 15:26:01 +0100

    Update resultats_recherche.php

--- resultats_recherche.php
+++ resultats_recherche.php
@@ -37,7 +37,7 @@ $conn->close();
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Résultats de la recherche</title>
-<style>
+    <style>
         body {
             font-family: Arial, sans-serif;
             color: #333;
